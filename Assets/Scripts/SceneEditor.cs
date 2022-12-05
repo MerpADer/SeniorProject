45f76d88298bf51b096b39339756bec0efdae244
D0gg3d <45501503+MerpADer@users.noreply.github.com>
2022-12-05 15:06:44 -0500

    Cutscene and transitions

---
+++ Assets/Scripts/SceneEditor.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneEditor : MonoBehaviour
+{
+    public Animator anim;
+
+    private GameObject sceneEnder;
+
+    private float timer = 0f;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        sceneEnder = FindSceneEnder();
+    }
+
+    private void Update()
+    {
+        if (sceneEnder == null)
+        {
+            anim.SetBool("IsFadedOut", true);
+            timer += Time.deltaTime;
+            if (timer >= 2f)
+            {
+                NextScene();
+                anim.SetBool("IsFadedOut", false);
+                FindSceneEnder();
+            }
+        }
+    }
+
+    private GameObject FindSceneEnder()
+    {
+        return GameObject.FindWithTag("SceneEnder");
+    }
+
+    //Scene Manipulation
+    public void NextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void NextScene(int SceneNum)
+    {
+        SceneManager.LoadScene(SceneNum);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
