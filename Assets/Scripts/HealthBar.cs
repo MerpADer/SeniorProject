255764865d2cf3ffa9da808e2f1e9febee888c0c
D0gg3d <45501503+MerpADer@users.noreply.github.com>
2023-02-15 23:02:34 -0500

    delete armor stuff

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -9,12 +9,8 @@ public class HealthBar : MonoBehaviour
     [SerializeField] Slider redSlider;
     [SerializeField] Slider red_WhiteSlider;
 
-    [SerializeField] Slider orangeSlider;
-    [SerializeField] Slider orange_WhiteSlider;
-
     // the whiteSlider starting value when WhiteHealthDecay starts
     private float redStartingWhiteValue;
-    private float orangeStartingWhiteValue;
 
     public void SetMaxHealth(int health, int armor)
     {
@@ -23,21 +19,14 @@ public class HealthBar : MonoBehaviour
 
         red_WhiteSlider.maxValue = health;
         red_WhiteSlider.value = health;
-
-        orangeSlider.maxValue = armor;
-        orangeSlider.value = armor;
-
-        orange_WhiteSlider.maxValue = armor;
-        orange_WhiteSlider.value = armor;
     }
 
-    public void SetHealth(int health, int armor)
+    public void SetHealth(int health)
     {
         // identifing if the slider val changed
         float startRedSliderVal = redSlider.value;
 
         redSlider.value = health;
-        orangeSlider.value = armor;
 
         //runs if hp changed
         if (startRedSliderVal != redSlider.value)
@@ -45,12 +34,7 @@ public class HealthBar : MonoBehaviour
             redStartingWhiteValue = red_WhiteSlider.value;
             InvokeRepeating(nameof(redWhiteHealthDecay), 0.3f, .01f);
         }
-        //runs if armor changed
-        else
-        {
-            orangeStartingWhiteValue = orange_WhiteSlider.value;
-            InvokeRepeating(nameof(orangeWhiteHealthDecay), 0.3f, .01f);
-        }
+
     }
 
     void redWhiteHealthDecay()
@@ -62,17 +46,6 @@ public class HealthBar : MonoBehaviour
         float min = redSlider.value;
         float decayVal = (redStartingWhiteValue - min) / 100;
         red_WhiteSlider.value -= decayVal;
-    }
-
-    void orangeWhiteHealthDecay()
-    {
-        if (orange_WhiteSlider.value <= orangeSlider.value)
-        {
-            CancelInvoke();
-        }
-        float min = orangeSlider.value;
-        float decayVal = (orangeStartingWhiteValue - min) / 100;
-        orange_WhiteSlider.value -= decayVal;
     }
 
 }
