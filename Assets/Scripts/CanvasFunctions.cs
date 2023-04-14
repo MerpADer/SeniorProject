baacf4b042db011186206282712a37420754f2e8
D0gg3d <45501503+MerpADer@users.noreply.github.com>
2023-04-14 16:12:16 -0400

    death button linked

--- Assets/Scripts/CanvasFunctions.cs
+++ Assets/Scripts/CanvasFunctions.cs
@@ -7,9 +7,18 @@ public class CanvasFunctions : MonoBehaviour
 
     [SerializeField] AudioSource audioSource;
 
+    private SceneEditor sceneEditor;
+
     public void PlaySound(AudioClip sound)
     {
         audioSource.PlayOneShot(sound);
+    }
+
+    public void LoadScene(int scene)
+    {
+        sceneEditor = FindObjectOfType<SceneEditor>();
+
+        sceneEditor.NextScene(scene);
     }
 
 }
